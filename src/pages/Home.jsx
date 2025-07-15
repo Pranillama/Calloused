7c5f9f2e4aabdb994edaf95843827eb3df33a499
Lama <juniyalama50@gmail.com>
2025-07-15 00:47:49 -0500

    changed the colors and designed the front page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,25 +1,36 @@
 //import { useState } from "react";
 import "../App.css";
+import frontImage from "../assets/front.jpg";
 import Button from "../components/Buttons";
 import { useNavigate } from "react-router-dom";
 function Home() {
   const navigate = useNavigate();
   return (
-    <div className="home-container">
-      <h1> CALLOUSED </h1>
-      <p className="quote" style={{ marginTop: 10 }}>
-        💪stay hard!
-      </p>
+    <section>
+      <div className="home-container">
+        <h1> Calloused </h1>
+        <p className="quote" style={{ marginTop: 10 }}>
+          Build your body. Build your mind. Track your journey.
+        </p>
 
-      <div className="exercise-section">
-        <Button
-          className="button--primary"
-          onClick={() => navigate("/workout")}
-        >
-          Exercises
-        </Button>
+        <div className="exercise-section">
+          <Button
+            className="button--primary"
+            onClick={() => navigate("/workout")}
+          >
+            Get Started
+          </Button>
+        </div>
       </div>
-    </div>
+
+      <div className="image">
+        <img
+          src={frontImage}
+          alt="Front"
+          style={{ float: "right", width: "80%", height: "auto" }}
+        />
+      </div>
+    </section>
   );
 }
 export default Home;
